6228962468f241c3b59aa01c2e18cc2667058f52
Anastasija <anastasijakoles@gmail.com>
2023-11-12 20:28:37 +0300

    feat: add Navlink instead of a to nav bar

--- src/components/navBar/menu/Menu.tsx
+++ src/components/navBar/menu/Menu.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './Menu.module.css'
+import { NavLink } from 'react-router-dom';
 
 export const Menu = () => {
   return (
     <ul className={s.menu_list}>
-      <li><a className={s.active} href='/profile'>Profile</a></li>
-      <li><a href='/messages'>Messages</a></li>
-      <li><a href='/news'>News</a></li>
-      <li><a href='/music'>Music</a></li>
-      <li><a href='/settings'>Settings</a></li>
+      <li><NavLink activeClassName={s.activeLink} to='/profile'>Profile</NavLink></li>
+      <li><NavLink activeClassName={s.activeLink} to='/messages'>Messages</NavLink></li>
+      <li><NavLink activeClassName={s.activeLink} to='/news'>News</NavLink></li>
+      <li><NavLink activeClassName={s.activeLink} to='/music'>Music</NavLink></li>
+      <li><NavLink activeClassName={s.activeLink} to='/settings'>Settings</NavLink></li>
     </ul>
   );
 };
