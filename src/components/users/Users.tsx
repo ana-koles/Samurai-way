cf95ad8bdfa9b47effc3982e755b2f545b267ba8
Anastasija <anastasijakoles@gmail.com>
2024-01-08 22:09:06 +0300

    feat: add button in User component to display users

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import s from './User.module.css'
 import { UsersContainerPropsType } from './UsersContainer';
 import catPhoto from '../../assets/friend4.jpg';
@@ -7,17 +7,6 @@ import photo from '../../assets/friend4.jpg';
 import axios from 'axios';
 import { UserType } from '../../redux/users-reducer';
 
-/* type UserType = {
-  name: string
-  id: number
-  uniqueUrlName: null
-  photos: {
-    small: null | string,
-    large: null | string
-},
-  status: null,
-  followed: boolean
-} */
 
 type UsersGetType = {
   items: UserType[]
@@ -27,62 +16,73 @@ type UsersGetType = {
 
 export const Users: React.FC<UsersContainerPropsType> = (props) => {
 
-  if (props.users.length === 0) {
-    axios.get<any>('https://social-network.samuraijs.com/api/1.0/users')
-    .then((response) => props.setUsers(response.data.items))
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get<UsersGetType>('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => props.setUsers(response.data.items))
 
-    /*   [
-        {
-        id: v1(),
-        photo: photo,
-        followed: true,
-        name: 'Whiskers',
-        status: `Missing my human right meow`,
-        location: {
-          city: 'Boston',
-          country: 'USA'
-        }
-        },
-        {
-        id: v1(),
-        photo: photo,
-        followed: false,
-        name: 'Oliver',
-        status: `Trying out a new toy today`,
-        location: {
-          city: 'London',
-          country: 'UK'
-        }
-        },
-        {
-        id: v1(),
-        photo: photo,
-        name: 'Leo',
-        followed: true,
-        status: `Knocked a glass off the table, just to see what would happen`,
-        location: {
-          city: 'Milan',
-          country: 'Italy'
-        }
-        },
-        {
-        id: v1(),
-        photo: photo,
-        name: 'Milo',
-        followed: false,
-        status: `Just had a gourmet meal of fresh salmon`,
-        location: {
-          city: 'Rome',
-          country: 'Italy'
-        }
-        }
-      ] */
-
+      /*   [
+          {
+          id: v1(),
+          photo: photo,
+          followed: true,
+          name: 'Whiskers',
+          status: `Missing my human right meow`,
+          location: {
+            city: 'Boston',
+            country: 'USA'
+          }
+          },
+          {
+          id: v1(),
+          photo: photo,
+          followed: false,
+          name: 'Oliver',
+          status: `Trying out a new toy today`,
+          location: {
+            city: 'London',
+            country: 'UK'
+          }
+          },
+          {
+          id: v1(),
+          photo: photo,
+          name: 'Leo',
+          followed: true,
+          status: `Knocked a glass off the table, just to see what would happen`,
+          location: {
+            city: 'Milan',
+            country: 'Italy'
+          }
+          },
+          {
+          id: v1(),
+          photo: photo,
+          name: 'Milo',
+          followed: false,
+          status: `Just had a gourmet meal of fresh salmon`,
+          location: {
+            city: 'Rome',
+            country: 'Italy'
+          }
+          }
+        ] */
+    }
   }
 
+/*   useEffect(() => {
+    if (props.users.length === 0) {
+      axios.get<UsersGetType>('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => props.setUsers(response.data.items))
+    }
+  }, [props.users])
+
+
+ */
 
   return (
     <div className={s.content}>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map(user => <div key={user.id} className={s.user_wrapper}>
           <div className={s.user_info}>
             <img src={user.photos.small != null ? user.photos.small : photo} alt="user" />
@@ -101,6 +101,7 @@ export const Users: React.FC<UsersContainerPropsType> = (props) => {
 
         </div>)}
     </div>
+
   );
 };
 
