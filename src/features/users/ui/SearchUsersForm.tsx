1962efd270e6f20432c658dafc0ab41b042244de
Anastasija <anastasijakoles@gmail.com>
2024-08-14 16:10:43 +0200

    refactor: remove comments in SearchUsersForm

--- src/features/users/ui/SearchUsersForm.tsx
+++ src/features/users/ui/SearchUsersForm.tsx
@@ -1,47 +1,10 @@
-import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { UserSearchFilterType } from "../model/users-reducer";
 
 type SearchUsersFormPropsType = {
   changeUserSearchFilter: (filter: UserSearchFilterType) => void;
 };
 
-
-/* export const _SearchUsersForm = ({changeUserSearchFilter}: SearchUsersFormPropsType) => {
-  return (
-    <Formik
-      initialValues={
-        {
-          term: '',
-          friend: false }
-      }
-      validate={validate}
-      onSubmit={(values) => {
-/*         validate(values); */
-/*         console.log(values)
-        changeUserSearchFilter(values.term)
-      }}
-      validateOnChange={false}
-      validateOnBlur={false}
-    >
-
-    {({ isSubmitting }) => (
-      <Form>
-        <Field type="text" name="term.term"/>
-        <ErrorMessage name="term.term" component="div" />
-        <Field type="checkbox" name="friend" />
-        <ErrorMessage name="friend" component="div" />
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-      </Form>
-    )}
-  </Formik> */
-
-/*   )  */
-
-/* } */
-
-/////////////////
 type UserFormParams = {
   term?: string;
   friend?: boolean
