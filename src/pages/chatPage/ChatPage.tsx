969c35450fb347a4c019d04e067292e5b470b4fc
Anastasija <anastasijakoles@gmail.com>
2024-08-21 19:04:32 +0200

    refactor: add message component with mock data

--- src/pages/chatPage/ChatPage.tsx
+++ src/pages/chatPage/ChatPage.tsx
@@ -1,4 +1,5 @@
 import s from './ChatPage.module.css'
+import avatar from '../../assets/friend2.jpg'
 
 const ChatPage = () => {
   return (
@@ -6,13 +7,30 @@ const ChatPage = () => {
       <Chat/>
       <AddChatMessageForm/>
     </div>
-
   )
 }
 
 const Chat = () => {
+  const messages = [1, 2, 3, 4]
+
   return (
-    <div>Chat page</div>
+    <div>{messages.map((message, index) => <Message key={index} />)}</div>
+  )
+}
+
+const Message = () => {
+  const message = {
+    avatar: avatar,
+    author: 'Khalisy',
+    text: 'Hi! I am Khalisy'
+  }
+
+  return (
+    <div style={{marginBottom: '20px'}}>
+      <img src={message.avatar} alt={'avatar'} style={{height: '30px', width: '30px'}}/>
+      <h4>{message.author}</h4>
+      <p>{message.text}</p>
+    </div>
   )
 }
 
