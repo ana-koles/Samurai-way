19393bec9ff4a9dddc3c6c2c7b9ff0bcb309cfa6
Anastasija <anastasijakoles@gmail.com>
2024-08-21 18:02:14 +0200

    refactor: delete header selectors

--- src/components/header/header-selectors.tsx
+++
@@ -1,9 +0,0 @@
-import { AppRootStateType } from "@/redux/redux-store";
-/*
-export const selectLogin = (state: AppRootStateType) => {
-  return state.auth.login
-}
-
-export const selectIsAuth = (state: AppRootStateType) => {
-  return state.auth.isAuth
-} */
